617ae070e587702e02bf8237c9fbcab0799770cb
Alona Zorina <alozorina@gmail.com>
2022-06-23 17:21:36 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import {Login} from './components/Login';
+import {Clock} from "./components/counter/Counter";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Andrew <code>src/App.js</code> and save to reload.
         </p>
+        <Clock></Clock>
         <a
           className="App-link"
           href="https://reactjs.org"
