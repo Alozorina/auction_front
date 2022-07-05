657859b8e78f7cf9ce4077057f361e7e03e89f5f
Alona Zorina <alozorina@gmail.com>
2022-07-05 15:53:03 +0200

    add NextUI library + dark theme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
-import { Outlet, Link } from "react-router-dom";
-import {Clock} from "./components/counter/Counter";
+import React from "react";
+import {Route, Routes} from "react-router-dom";
+import {Register} from "./components/Register";
+import {Login} from "./components/Login";
+import {Profile} from "./components/Profile";
+import {Container, createTheme, NextUIProvider} from "@nextui-org/react"
+import {ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const darkTheme = createTheme({
+    type: 'dark',
+    theme: {
+        colors: {
+            // brand colors
+            background: '#253645',
+            text: '#fff',
+            inputColor: '#384857',
+            selection: '#151D25',
+            accents0: '#2B3B4A',
+            border: '$blue800',
+            primaryLight: '#7fb6f5',
+        },
+        space: {
+            5: '13px'
+        },
+        fonts: {
+            sans: '"Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif',
+        },
+        letterSpacings: {
+            tighter: '0',
+            tight: '0',
+            normal: '0',
+            wide: '0.025em',
+            wider: '0.05em',
+            widest: '0.1em'
+        }
+    }
+})
 
 function App() {
-  return (
-      <div>
-          <h1>Bookkeeper</h1>
-          <nav
-              style={{
-                  borderBottom: "solid 1px",
-                  paddingBottom: "1rem",
-              }}
-          >
-              <Link to="/components">Invoices</Link> |{" "}
-              <Link to="/components">Expenses</Link>
-          </nav>
-          <Outlet />
-      </div>
-  );
+    return (
+        <NextUIProvider theme={darkTheme }>
+            <Container fluid>
+                <Routes>
+                    <Route exact path="/login" element={<Login />} />
+                    <Route exact path="/register" element={<Register />} />
+                    <Route exact path="/profile" element={<Profile />} />
+                </Routes>
+            </Container>
+            <ToastContainer />
+        </NextUIProvider>
+    );
 }
-
 
 export default App;
