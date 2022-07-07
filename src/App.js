2f52e217037692d149f47047ed1fe41800b8bf73
Alona Zorina <alozorina@gmail.com>
2022-07-08 01:59:22 +0200

    import basic fonts and styles

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import {Profile} from "./components/Profile";
 import {Container, createTheme, NextUIProvider} from "@nextui-org/react"
 import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import {Home} from "./components/Home";
+import {Header} from "./components/Header";
 
 const darkTheme = createTheme({
     type: 'dark',
@@ -18,14 +20,15 @@ const darkTheme = createTheme({
             inputColor: '#384857',
             selection: '#151D25',
             accents0: '#2B3B4A',
-            border: '$blue800',
+            border: '#fff',
             primaryLight: '#7fb6f5',
+            secondaryLight: '#FFC300'
         },
         space: {
             5: '13px'
         },
         fonts: {
-            sans: '"Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif',
+            sans: "Montserrat",
         },
         letterSpacings: {
             tighter: '0',
@@ -34,15 +37,25 @@ const darkTheme = createTheme({
             wide: '0.025em',
             wider: '0.05em',
             widest: '0.1em'
-        }
+        },
+        breakpoints: {
+            xs: '520px',
+            sm: '720px',
+            md: '960px',
+            lg: '1400px',
+            xl: '1920px'
+        },
+
     }
 })
 
 function App() {
     return (
         <NextUIProvider theme={darkTheme }>
-            <Container fluid>
+            <Header></Header>
+            <Container xl>
                 <Routes>
+                    <Route exact path="/" element={<Home />} />
                     <Route exact path="/login" element={<Login />} />
                     <Route exact path="/register" element={<Register />} />
                     <Route exact path="/profile" element={<Profile />} />
