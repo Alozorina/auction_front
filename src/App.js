3a378f8a77d1425e4f0abaa02e9cd96e0dea5fee
Alona Zorina <alozorina@gmail.com>
2022-07-10 23:20:49 +0200

    add QueryClientProvider

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ import {Profile} from "./components/Profile";
 import {Container, createTheme, NextUIProvider} from "@nextui-org/react"
 import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import {QueryClient, QueryClientProvider} from "react-query";
 import {Home} from "./components/Home";
 import {Header} from "./components/Header";
+import {ItemPage} from "./components/ItemPage";
 
 const darkTheme = createTheme({
     type: 'dark',
@@ -22,7 +24,8 @@ const darkTheme = createTheme({
             accents0: '#2B3B4A',
             border: '#fff',
             primaryLight: '#7fb6f5',
-            secondaryLight: '#FFC300'
+            secondaryLight: '#FFC300',
+            backgroundContrast: '#384857'
         },
         space: {
             5: '13px'
@@ -49,20 +52,25 @@ const darkTheme = createTheme({
     }
 })
 
+const queryClient = new QueryClient();
+
 function App() {
     return (
-        <NextUIProvider theme={darkTheme }>
-            <Header></Header>
-            <Container xl>
-                <Routes>
-                    <Route exact path="/" element={<Home />} />
-                    <Route exact path="/login" element={<Login />} />
-                    <Route exact path="/register" element={<Register />} />
-                    <Route exact path="/profile" element={<Profile />} />
-                </Routes>
-            </Container>
-            <ToastContainer />
-        </NextUIProvider>
+        <QueryClientProvider client={queryClient}>
+            <NextUIProvider theme={darkTheme}>
+                <Header></Header>
+                <Container xl>
+                    <Routes>
+                        <Route exact path="/" element={<Home/>}/>
+                        <Route path="/auction/:id" element={<ItemPage/>}/>
+                        <Route exact path="/login" element={<Login/>}/>
+                        <Route exact path="/register" element={<Register/>}/>
+                        <Route exact path="/profile" element={<Profile/>}/>
+                    </Routes>
+                </Container>
+                <ToastContainer/>
+            </NextUIProvider>
+        </QueryClientProvider>
     );
 }
 
