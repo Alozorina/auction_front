4b7bb93e5fac6a8a1d65f19369ccc5c041d54200
Alona Zorina <alozorina@gmail.com>
2022-07-10 23:25:18 +0200

    add ItemPage component

---
+++ src/components/ItemPage.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {useParams} from "react-router-dom";
+import {useQuery} from "react-query";
+import {getAllSortedByStartDate} from "../services/item.service";
+import {ItemProfile} from "./ItemProfile";
+
+export const ItemPage = () => {
+    const {isLoading, error, data: items, isFetching} = useQuery("auctionsData", () =>
+        getAllSortedByStartDate());
+    let {id} = useParams();
+    if(!items)
+        return <div>Loading...</div>;
+    const item = items.find(i => i.id == id);
+    if(!item)
+        return <div>Item not found</div>;
+    return (
+        item && <ItemProfile item={item}></ItemProfile>
+    )
+}
