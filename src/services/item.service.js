c3d6a2365f32a031544f02df61dadd7133cadfc6
Alona Zorina <alozorina@gmail.com>
2022-07-14 23:07:18 +0300

    add updateBid()

--- src/services/item.service.js
+++ src/services/item.service.js
@@ -5,6 +5,15 @@ const API_URL = "https://localhost:5001/api/item/";
 export const getAllSortedByStartDate = async () => {
     return await axios.get(API_URL)
         .then(resp => resp.data);
-    }
+};
+
+export const updateBid = async (itemId, currentBid, buyerId) => {
+    console.log({itemId, currentBid, buyerId})
+    return axios.put(API_URL + itemId, {
+        currentBid, buyerId})
+        .then((response) => {
+            return response.data;
+        });
+}
 
 export const IMAGE_PATH = "https://localhost:5001/StaticFiles/images/";
