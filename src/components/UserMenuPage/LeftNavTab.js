077b6219c9eb5004a9e27c4086de0dcb07a18b1a
Alona Zorina <alozorina@gmail.com>
2022-08-06 18:29:09 +0300

    separate navigation panel to new file

---
+++ src/components/UserMenuPage/LeftNavTab.js
@@ -0,0 +1,49 @@
+import React from "react";
+import {Card, Grid} from "@nextui-org/react";
+import {NavLink} from "react-router-dom";
+
+export const LeftNavTab = () => {
+    return (
+        <Grid.Container gap={4} justify="left" css={{alignContent: 'start'}}>
+            <Grid xs={12}>
+                <NavLink
+                    to="/profile"
+                    style={({isActive}) => ({
+                        color: isActive ? "#FFC300" : "#fff"
+                    })}>
+                    PROFILE
+                </NavLink>
+            </Grid>
+            <Card.Divider css={{width: '95%'}}/>
+            <Grid xs={12}>
+                <NavLink
+                    to="/login"
+                    style={({isActive}) => ({
+                        color: isActive ? "#FFC300" : "#fff"
+                    })}>
+                    MY LOTS
+                </NavLink>
+            </Grid>
+            <Card.Divider css={{width: '95%'}}/>
+            <Grid xs={12}>
+                <NavLink
+                    to="/login"
+                    style={({isActive}) => ({
+                        color: isActive ? "#FFC300" : "#fff"
+                    })}>
+                    MY PURCHASES
+                </NavLink>
+            </Grid>
+            <Card.Divider css={{width: '95%'}}/>
+            <Grid xs={12}>
+                <NavLink
+                    to="/addlot"
+                    style={({isActive}) => ({
+                        color: isActive ? "#FFC300" : "#fff"
+                    })}>
+                    ADD LOT
+                </NavLink>
+            </Grid>
+        </Grid.Container>
+    )
+}
