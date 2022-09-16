802be692dd2c5253c8c0acd7172cddb058b0b8e5
Alona <alona.zorina@nure.ua>
2022-09-17 01:43:52 +0300

    add log out and search implementation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,19 @@
-import React, {useEffect, useState} from "react";
-import {useNavigate} from "react-router-dom";
+import React, {useContext, useEffect, useState} from "react";
+import {Link, useNavigate} from "react-router-dom";
 import './Header.css';
 import Logo from "../svg/logo.svg";
 import Search from "../svg/search.svg";
 import ShoppingCart from "../svg/shopping-cart.svg";
-import {Input, Link, User} from "@nextui-org/react"
+import {Input, User, Tooltip, Button} from "@nextui-org/react"
 import {useAccessManager} from "../services/authorization.service";
+import AuthenticationService from "../services/authentication.service";
+import {AppContext} from "./AppContext";
 
 const useScrollDirection = () => {
     const [scrollDirection, setScrollDirection] = useState(null);
 
-
     useEffect(() => {
         let lastScrollY = window.scrollY;
-
         const updateScrollDirection = () => {
             const scrollY = window.scrollY;
             const direction = scrollY > lastScrollY ? "down" : "up";
@@ -32,18 +32,33 @@ const useScrollDirection = () => {
 };
 
 export const Header = () => {
-    const navigate = useNavigate();
     const scrollDirection = useScrollDirection();
+    const {setUser} = useContext(AppContext);
     let {user} = useAccessManager();
     const [searchString, setSearchString] = useState("");
 
     const onChangeSearch = (e) => setSearchString(e.target.value);
+    const navigate = useNavigate();
+    let searchUrl = "/search?query=" + searchString;
+
+    const handleSearch = (e) => {
+        if (e.key === 'Enter') {
+            navigate(searchUrl)
+        }
+    }
+
+    let logOutBtn = <Button onClick={() => {
+        AuthenticationService.logout();
+        setUser(null)
+    }}>
+        LOG OUT
+    </Button>;
 
     return (
         <div className={`navBar ${scrollDirection === "down" ? "hidden" : "visible"}`}>
             <div className='wrapper'>
                 <div className={'navItem'}>
-                    <Link onClick={() => navigate('/')}>
+                    <Link to='/'>
                         <img src={Logo} className="logo" alt="icon"/>
                     </Link>
                 </div>
@@ -64,23 +79,35 @@ export const Header = () => {
                            contentRightStyling={false}
                            placeholder="Search Auction"
                            onChange={onChangeSearch}
+                           onKeyDown={handleSearch}
                            contentRight={
-                               <Link href={"/search?query="+searchString}> <img src={Search} id="search" alt="icon"/></Link>
+                               searchString ?
+                                   <Link to={searchUrl}> <img src={Search} id="search"
+                                                              alt="icon"/></Link>
+                                   : <img src={Search} id="search" alt="icon"/>
                            }
                     />
                     <img src={ShoppingCart} id="cart" alt="icon"/>
                 </div>
+
                 {user && <div className={'navItem'}>
-                    <Link onClick={() => navigate("/profile")}>
+                    <Link to="/profile">
+                        <Tooltip
+                            trigger="hover"
+                            placement="bottom"
+                            hideArrow
+                            content={logOutBtn}>
                         <User
                             name={user.firstName + " " + user.lastName}
                             squared
                             text={user.firstName[0]}
                         />
-                    </Link>
-                </div>}
+                    </Tooltip>
+                </Link>
+                    </div>}
+
                 {!user && <div className={'navItem'}>
-                    <Link onClick={() => navigate("/login")}>
+                    <Link to="/login">
                         LOG IN
                     </Link>
                 </div>}
