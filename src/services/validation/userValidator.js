4e9822c5d04b85d0dc2158d85da922a0cc52643c
Alona <alona.zorina@nure.ua>
2022-10-07 14:20:42 +0300

    updated validate Password Length

--- src/services/validation/userValidator.js
+++ src/services/validation/userValidator.js
@@ -4,7 +4,7 @@ export const validateNameLength = (value) =>
     between(2, 50)(value);
 export const validateEmailSyntax = (value) => !value.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i) && "Enter correct email";
 export const validatePasswordLength = (value) =>
-    between(6,32)(value);
+    between(8,32)(value);
 export const validateEqual = (value, password) => {
     if (value !== password) {
         return (
