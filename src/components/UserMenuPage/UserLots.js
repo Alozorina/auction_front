e16af28661165de89c79648dd88bb2df1aa86ed6
Alona <alona.zorina@nure.ua>
2022-10-07 16:21:22 +0300

    add redirect to login page

--- src/components/UserMenuPage/UserLots.js
+++ src/components/UserMenuPage/UserLots.js
@@ -3,7 +3,7 @@ import {useQuery} from "react-query";
 import {getLots} from "../../services/item.service";
 import {Gallery} from "../Item/Gallery";
 import {Text, Grid, Container, Spacer} from "@nextui-org/react"
-import {useAccessManager} from "../../services/authorization.service";
+import {useAccessManager, RequireAuth} from "../../services/authorization.service";
 import {LeftNavTab} from "./LeftNavTab";
 
 export const UserLots = () => {
@@ -14,24 +14,26 @@ export const UserLots = () => {
     let bottomText = !items?.length && "You don't have any lots";
 
     return (
-        <Grid.Container gap={1} justify="left">
-            <Grid md={3} sm={4} xs={0} css={{paddingLeft: "2%", paddingTop: "30px", backgroundColor: '#2d3e4e'}}>
-                <LeftNavTab></LeftNavTab>
-            </Grid>
-            <Grid md={9} sm={8} xs={12} css={{paddingLeft: "2%"}}>
-                {user &&
-                    <Container css={{
-                        margin: 'auto',
-                        paddingTop: '2%',
-                    }}>
-                        <Spacer y={1.2}/>
-                        <Gallery
-                            itemList={items ?? []}
-                            header={'MY LOTS'}>
-                        </Gallery>
-                        <Text b size="$xl" css={{marginLeft: "auto"}}>{bottomText}</Text>
-                    </Container>}
-            </Grid>
-        </Grid.Container>
+        <RequireAuth roles={["User", "Admin"]}>
+            <Grid.Container gap={1} justify="left">
+                <Grid md={3} sm={4} xs={0} css={{paddingLeft: "2%", paddingTop: "30px", backgroundColor: '#2d3e4e'}}>
+                    <LeftNavTab></LeftNavTab>
+                </Grid>
+                <Grid md={9} sm={8} xs={12} css={{paddingLeft: "2%"}}>
+                    {user &&
+                        <Container css={{
+                            margin: 'auto',
+                            paddingTop: '2%',
+                        }}>
+                            <Spacer y={1.2}/>
+                            <Gallery
+                                itemList={items ?? []}
+                                header={'MY LOTS'}>
+                            </Gallery>
+                            <Text b size="$xl" css={{marginLeft: "auto"}}>{bottomText}</Text>
+                        </Container>}
+                </Grid>
+            </Grid.Container>
+        </RequireAuth>
     );
 };
--- src/components/UserMenuPage/UserPurchases.js
+++ src/components/UserMenuPage/UserPurchases.js
@@ -3,7 +3,7 @@ import {useQuery} from "react-query";
 import {getPurchases} from "../../services/item.service";
 import {Gallery} from "../Item/Gallery";
 import {Text, Grid, Container, Spacer} from "@nextui-org/react"
-import {useAccessManager} from "../../services/authorization.service";
+import {RequireAuth, useAccessManager} from "../../services/authorization.service";
 import {LeftNavTab} from "./LeftNavTab";
 
 export const UserPurchases = () => {
@@ -14,24 +14,26 @@ export const UserPurchases = () => {
     let bottomText = !items?.length && "You don't have any purchases";
 
     return (
-        <Grid.Container gap={1} justify="left">
-            <Grid md={3} sm={4} xs={0} css={{paddingLeft: "2%", paddingTop: "30px", backgroundColor: '#2d3e4e'}}>
-                <LeftNavTab></LeftNavTab>
-            </Grid>
-            <Grid md={9} sm={8} xs={12} css={{paddingLeft: "2%"}}>
-                {user &&
-                    <Container css={{
-                        margin: 'auto',
-                        paddingTop: '2%',
-                    }}>
-                        <Spacer y={1.2}/>
-                        <Gallery
-                            itemList={items ?? []}
-                            header={'MY PURCHASES'}>
-                        </Gallery>
-                        <Text b size="$xl" css={{marginLeft: "auto"}}>{bottomText}</Text>
-                    </Container>}
-            </Grid>
-        </Grid.Container>
+        <RequireAuth roles={["User", "Admin"]}>
+            <Grid.Container gap={1} justify="left">
+                <Grid md={3} sm={4} xs={0} css={{paddingLeft: "2%", paddingTop: "30px", backgroundColor: '#2d3e4e'}}>
+                    <LeftNavTab></LeftNavTab>
+                </Grid>
+                <Grid md={9} sm={8} xs={12} css={{paddingLeft: "2%"}}>
+                    {user &&
+                        <Container css={{
+                            margin: 'auto',
+                            paddingTop: '2%',
+                        }}>
+                            <Spacer y={1.2}/>
+                            <Gallery
+                                itemList={items ?? []}
+                                header={'MY PURCHASES'}>
+                            </Gallery>
+                            <Text b size="$xl" css={{marginLeft: "auto"}}>{bottomText}</Text>
+                        </Container>}
+                </Grid>
+            </Grid.Container>
+        </RequireAuth>
     );
 };
